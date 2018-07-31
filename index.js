610abf7eff20abeef3170a0fea5454d7a25901ce
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2018-07-31 14:39:32 -0400

    modified index.html

--- index.js
+++ index.js
@@ -1,12 +1,62 @@
 const http = require('http'); // inserting the http module 
+const fs = require("fs"); // inserting the file system module
+const path = require("path"); // inserting the path module
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+  let filePath = "." + req.url;
+
+  if(filePath == "./") {
+    filePath = "./index.html";
+  }
+
+  let extName = path.extname(filePath);
+  let contentType = "text/html";
+
+  switch(extName) {
+    case ".js":
+      contentType = "text/javascript";
+    break;
+    case ".css":
+     contentType = "text/css";
+    break;
+    case ".json":
+      contentType = "application/json";
+    break;
+    case ".png":
+      contentType = "image/png";
+    break;
+    case ".jpg":
+      contentType = "image/jpg";
+    break;
+    case ".wav":
+      contentType = "audio/wav";
+    break;
+  }
+
+  fs.readFile(filePath, function(error, content){
+    if(error) {
+      if(error.code == "ENOENT") {
+        fs.readFile("./404.html", function(error, content){
+          res.writeHead(200, { 'Content-Type': contentType});
+          res.end(content, "utf-8");
+        });
+      }
+      else {
+        res.writeHead(500);
+        res.end("Sorry, server Error!\n");
+        res.end();
+      }
+    }
+    else {
+      res.writeHead(200, {'Content-Type': contentType});
+      res.end(content, "utf-8");
+    }
+  });
+
+
 });
 
 server.listen(port, hostname, () => {
